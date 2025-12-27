833833c67e4452e3d150f2b87e1ba35c456500a2
denis <barondenis77@gmail.com>
2025-12-27 14:27:04 +0300

    третий коммит

--- src/FilterCafes.jsx
+++ src/FilterCafes.jsx
@@ -1,44 +1,37 @@
-import React from "react";
-const FilterCafes = ({onFilterChange}) => {
-    const subwayOptions = [
-        {
-            name: "Арбатская",
-            code: "Arbat",
-        },
-        {
-            name: "Александровский сад",
-            code: "Alexanders Garden",
-        },
-        {
-            name: "Московская",
-            code: "Moscow",
-        },
-        {
-            name: "Парк Культуры",
-            code: "Culture",
-        },
-        {
-            name: "Театральная",
-            code: "Theater",
-        },
-    ];
-    const handleChange = (event) => {
-        if(onFilterChange) {
-            onFilterChange(event.target.value);
-        }
-    };
+import React from 'react';
 
-    return (
-        <div className="controls">
-            <select name="subway" id="subway" onChange={handleChange}>
-                <option value="All" selected>Все</option>
-                {subwayOptions.map((option) => (
-                    <option key={option.code} value={option.code}>
-                        {option.name}
-                    </option>
-                ))}
-            </select>
-        </div>
-    );
+const FilterCafes = ({ onFilterChange, value }) => {
+  const subwayOptions = [
+    { name: "Арбатский", code: "Arbat" },
+    { name: "Александровский сад", code: "Alexanders Garden" },
+    { name: "Московская", code: "Moscow" },
+    { name: "Парк Культуры", code: "Culture" },
+    { name: "Театральная", code: "Theater" },
+  ];
+
+  const handleChange = (event) => {
+    if (onFilterChange) {
+      onFilterChange(event.target.value);
+    }
+  };
+
+  return (
+    <div className="controls">
+      <select
+        id="subway"
+        name="subway"
+        value={value}
+        onChange={handleChange}
+      >
+        <option value="All">Все</option>
+        {subwayOptions.map((option) => (
+          <option key={option.code} value={option.code}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 };
+
 export default FilterCafes;
